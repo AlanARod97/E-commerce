84cf131b4694605595eac109c76faaadcbf0ecc1
Alan Andres Rodriguez <rodriguez.alanandres@gmail.com>
2023-02-19 19:24:56 -0300

    19/02/2023

--- js/app.js
+++ js/app.js
@@ -5,129 +5,178 @@ const productos = [
         id: "reloj-francia",
         titulo: "Reloj Francia",
         imagen: "./img/RelojMujer-productos1.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Mujer",
-            id:"mujer"
+            id: "mujer"
         },
-        precio:1500
+        precio: 1500
 
     },
 
     {
-     
+
         id: "reloj-heidi",
         titulo: "Reloj Heidi",
         imagen: "./img/RelojMujer-productos2.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Mujer",
-            id:"mujer"
+            id: "mujer"
         },
-        precio:2000  
+        precio: 2000
     },
 
     {
-        
+
         id: "reloj-shirley",
         titulo: "Reloj Shirley",
         imagen: "./img/RelojMujer-productos3.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Mujer",
-            id:"mujer"
+            id: "mujer"
         },
-        precio:2000
+        precio: 2000
     },
 
     {
-        
+
         id: "reloj-mile",
         titulo: "Reloj Mile",
         imagen: "./img/RelojMujer-Producto4.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Mujer",
-            id:"mujer"
+            id: "mujer"
         },
-        precio:2500
+        precio: 2500
     },
 
     {
-        
+
         id: "reloj-italia",
         titulo: "Reloj Italia",
         imagen: "./img/RelojMujer-Producto5.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Mujer",
-            id:"mujer"
+            id: "mujer"
         },
-        precio:2000
+        precio: 2000
     },
 
+    {
+
+        id: "reloj-pink",
+        titulo: "Reloj Pink",
+        imagen: "./img/RelojMujer-Producto6.jpg.jpg",
+        categoria: {
+            nombre: "Relojes de Mujer",
+            id: "mujer"
+        },
+        precio: 2000
+    },
+
+    {
+
+        id: "reloj-shinning",
+        titulo: "Reloj Shinning",
+        imagen: "./img/RelojMujer-Producto7.jpg.jpg",
+        categoria: {
+            nombre: "Relojes de Mujer",
+            id: "mujer"
+        },
+        precio: 2000
+    },
+
+    {
+
+        id: "reloj-sweet-white",
+        titulo: "Reloj Sweet White",
+        imagen: "./img/RelojMujer-Producto8.jpg.jpg",
+        categoria: {
+            nombre: "Relojes de Mujer",
+            id: "mujer"
+        },
+        precio: 2000
+    },
+
+    {
+
+        id: "reloj-world",
+        titulo: "Reloj World",
+        imagen: "./img/RelojMujer-Producto9.jpg.jpg",
+        categoria: {
+            nombre: "Relojes de Mujer",
+            id: "mujer"
+        },
+        precio: 2000
+    },
 
     /*Hombre*/
 
     {
-        
+
         id: "reloj-land",
         titulo: "Reloj Land",
         imagen: "./img/RelojHombre-productos1.0.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Hombre",
-            id:"hombre"
+            id: "hombre"
         },
-        precio:2000
+        precio: 2000
     },
 
     {
-        
+
         id: "reloj-milo",
         titulo: "Reloj Milo",
         imagen: "./img/RelojHombre-productos2.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Hombre",
-            id:"hombre"
+            id: "hombre"
         },
-        precio:2500
+        precio: 2500
     },
 
     {
-        
+
         id: "reloj-iron",
         titulo: "Reloj Iron",
         imagen: "./img/RelojHombre-productos3.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Hombre",
-            id:"hombre"
+            id: "hombre"
         },
-        precio:3000
+        precio: 3000
     },
 
     {
-        
+
         id: "reloj-silver",
         titulo: "Reloj Silver",
         imagen: "./img/RelojHombre-producto4.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Hombre",
-            id:"hombre"
+            id: "hombre"
         },
-        precio:2500
+        precio: 2500
 
     },
 
     {
-        
+
         id: "reloj-milotwo",
         titulo: "Reloj MiloTwo",
         imagen: "./img/RelojHombre-producto8.jpg",
-        categoria:{
+        categoria: {
             nombre: "Relojes de Hombre",
-            id:"hombre"
+            id: "hombre"
         },
-        precio:2300
+        precio: 2300
     },
-    
+
 
 
 ]
+
+
 
 const contenedor_productos = document.querySelector("#contenedorProductos");
 const botonesCategoria = document.querySelectorAll(".botonCategoria");
@@ -135,11 +184,11 @@ const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
 const numerito = document.querySelector("#numerito");
 
-function cargarProductos(productosElegidos){
+function cargarProductos(productosElegidos) {
 
     contenedor_productos.innerHTML = "";
-    productosElegidos.forEach(producto =>{
-        const div =document.createElement("div");
+    productosElegidos.forEach(producto => {
+        const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `<img src="${producto.imagen}" alt="${producto.titulo}" class="producto-imagen">
         <div class="detallesProductos">
@@ -159,71 +208,102 @@ function cargarProductos(productosElegidos){
 cargarProductos(productos)
 
 botonesCategoria.forEach(boton => {
-    boton.addEventListener("click", (e)=>{
-        
+    boton.addEventListener("click", (e) => {
+
         botonesCategoria.forEach(boton => boton.classList.remove("active"))
         e.currentTarget.classList.add("active");
 
-        if(e.currentTarget.id != "todos")
-        {
+        if (e.currentTarget.id != "todos") {
             const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id)
 
-            tituloPrincipal.innerText= productoCategoria.categoria.nombre;
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
             const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id)
             cargarProductos(productosBoton)
-        }else{
-            tituloPrincipal.innerText= "Todos los productos";
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
             cargarProductos(productos)
         }
-        
-        
+
+
     })
 });
 
-function actualizarBotonesAgregar(){
+function actualizarBotonesAgregar() {
     botonesAgregar = document.querySelectorAll(".producto-agregar");
 
-    botonesAgregar.forEach(boton =>{
+    botonesAgregar.forEach(boton => {
         boton.addEventListener("click", agregarAlCarrito);
     });
-} 
-let productosEnCarrito
-const productosEnCarritoLS =  JSON.parse(localStorage.getItem("productos-en-carrito"));
-if(productosEnCarritoLS){
-     productosEnCarrito = productosEnCarritoLS
-}else{
-    productosEnCarrito = []
 }
 
 
-function agregarAlCarrito(e){
+let productosEnCarrito
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+
+function agregarAlCarrito(e) {
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
 
-    if(productosEnCarrito.some(producto => producto.id === idBoton)){
-    const index =   productosEnCarrito.findIndex(producto => producto.id === idBoton);
-    productosEnCarrito[index].cantidad++;
-    }else{
-        productoAgregado.cantidad =1;
+    if (productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
         productosEnCarrito.push(productoAgregado);
 
 
     }
-    
+
 
     actualizarNumerito();
 
-     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 
-   
+
 
 }
 
-function actualizarNumerito(){
-    let nuevoNumerito = productosEnCarrito.reduce((acc,producto)=> acc + producto.cantidad, 0);
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
 }
 
+
+
+
+
+
+/*API REST*/
+
+fetch('https://api.chucknorris.io/jokes/random')
+.then((response ) => {return response.json()})
+.then((data) =>{
+    const logArt = document.getElementById("logoArt");
+    logArt.addEventListener("click", ()=>{
+            
+        Toastify({
+
+            text: `${data.value}`,
+            style: {
+                background:"linear-gradient(to bottom, #F2DED4, #E8C6B6,#DAAB96)",
+              },
+         
+            duration: 3000,
+            
+            }).showToast();
+    });
+
+    
+}
+)
 
 
 
--- js/carrito.js
+++ js/carrito.js
@@ -1,19 +1,25 @@
-const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
-console.log(productosEnCarrito)
+let productosEnCarrito = localStorage.getItem("productos-en-carrito");
+productosEnCarrito = JSON.parse(productosEnCarrito);
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones= document.querySelector("#carrito-acciones");
 const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
+const botonComprar = document.querySelector("#carrito-acciones-comprar");
 
+function cargarProductosCarrito(){
+if(productosEnCarrito && productosEnCarrito.length > 0){
 
-if(productosEnCarrito){
+   
+
     contenedorCarritoVacio.classList.add("disabled")
     contenedorCarritoProductos.classList.remove("disabled")
     contenedorCarritoAcciones.classList.remove("disabled")
     contenedorCarritoComprado.classList.add("disabled")
 
-     contenedorCarritoProductos.innerHTML = " "
+    contenedorCarritoProductos.innerHTML = " ";
 
     productosEnCarrito.forEach(producto =>{
         const div = document.createElement("div")
@@ -47,5 +53,104 @@ if(productosEnCarrito){
     
 
 }else{
-    
+    contenedorCarritoVacio.classList.remove("disabled")
+    contenedorCarritoProductos.classList.add("disabled")
+    contenedorCarritoAcciones.classList.add("disabled")
+    contenedorCarritoComprado.classList.add("disabled") 
 }
+
+actualizarBotonesEliminar();
+actualizarTotal()
+
+}
+
+cargarProductosCarrito();
+
+
+
+function actualizarBotonesEliminar(){
+    botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+
+    botonesEliminar.forEach(boton =>{
+        boton.addEventListener("click", eliminarDelCarrito);
+    });
+} 
+
+
+function eliminarDelCarrito(e){
+    const idBoton = e.currentTarget.id;
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    
+    
+    productosEnCarrito.splice(index, 1);
+    cargarProductosCarrito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+}
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+  
+
+function vaciarCarrito(){
+
+    Swal.fire(
+        {
+            icon: "warning",
+            title:"Se vaciará el carrito de compras",
+            text:"desea continuar?",
+            showCancelButton:true,
+            confirmButtonText:"Vaciar Carrito",
+            confirmButtonColor:"#961818",
+            cancelButtonText:"Cancelar",
+        }
+    ).then((result)=>{
+        if(result.value){
+            productosEnCarrito.length =0;
+            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+            cargarProductosCarrito()
+        }
+    })
+
+  
+
+};
+
+function actualizarTotal(){
+
+    const totalCalculado  = productosEnCarrito.reduce((acc, producto)=> acc + (producto.cantidad * producto.precio), 0)
+    total.innerText =  `$${totalCalculado};`;
+};
+
+
+
+botonComprar.addEventListener("click", comprarCarrito);
+  
+
+function comprarCarrito(){
+
+    Swal.fire(
+        {
+            icon: "success",
+            title:"Esta a punto de comprar",
+            text:"desea continuar?",
+            showCancelButton:true,
+            confirmButtonText:"Comprar Producto/s",
+            confirmButtonColor:"green",
+            cancelButtonText:"Cancelar",
+        }
+    ).then((result)=>{
+        if(result.value){
+            productosEnCarrito.length =0;
+            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+            contenedorCarritoVacio.classList.add("disabled")
+            contenedorCarritoProductos.classList.add("disabled")
+            contenedorCarritoAcciones.classList.add("disabled")
+            contenedorCarritoComprado.classList.remove("disabled")
+           
+        }
+    })
+
+  
+
+};
